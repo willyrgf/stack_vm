28a0f25afb73c3aa379026b61f0bfbc05d9b67f4
willyrgf <willyr.goncalves@gmail.com>
2022-02-08 21:34:20 -0300

    add unit tests to stack impl

--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -17,3 +17,38 @@ impl<T> Stack<T> {
         self.0.pop().expect("unable to pop from empty stack")
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::Stack;
+
+    #[test]
+    fn new() {
+        let stack: Stack<usize> = Stack::new();
+        assert!(stack.is_empty())
+    }
+
+    #[test]
+    fn push() {
+        let mut stack: Stack<usize> = Stack::new();
+        stack.push(7);
+        assert!(!stack.is_empty())
+    }
+
+    #[test]
+    fn pop() {
+        const N: usize = 7;
+        let mut stack: Stack<usize> = Stack::new();
+
+        stack.push(N);
+        let v = stack.pop();
+        assert_eq!(v, N);
+    }
+
+    #[test]
+    #[should_panic(expected = "empty stack")]
+    fn is_empty() {
+        let mut stack: Stack<usize> = Stack::new();
+        stack.pop();
+    }
+}
