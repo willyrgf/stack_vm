308319ce7952ed6218ed6e0d3ae877c51ffbfb00
willyrgf <willyr.goncalves@gmail.com>
2022-02-10 19:24:05 -0300

    add impl and test of a init to instruction

--- src/instruction/mod.rs
+++ src/instruction/mod.rs
@@ -19,3 +19,32 @@ impl<T: fmt::Debug> fmt::Debug for Instruction<T> {
         )
     }
 }
+
+impl<T: fmt::Debug> Instruction<T> {
+    fn new(op_code: usize, op_name: &str, arity: usize, fun: InstructionFn<T>) -> Self {
+        Instruction {
+            op_code,
+            op_name: String::from(op_name),
+            arity,
+            fun,
+        }
+    }
+}
+
+mod test {
+    use super::Instruction;
+    use crate::machine::Machine;
+
+    #[derive(Debug)]
+    struct Operand(i64);
+
+    fn noop(_machine: &mut Machine<Operand>, _args: &[usize]) {}
+
+    #[test]
+    fn new() {
+        let operand = Instruction::new(7, "noop", 3, noop);
+        assert_eq!(operand.op_code, 7);
+        assert_eq!(operand.op_name, "noop".to_string());
+        assert_eq!(operand.arity, 3);
+    }
+}
