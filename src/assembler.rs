c81c8cfe7fbe2514fd2f193ece13400af176eac4
willyrgf <willyr.goncalves@gmail.com>
2022-02-12 18:23:06 -0300

    [wip] implementing a assembler

---
+++ src/assembler.rs
@@ -0,0 +1,63 @@
+use std::{fmt, ptr::NonNull, str::ParseBoolError, sync::Arc};
+
+use crate::{instruction_table::InstructionTable, write_once_table::WriteOnceTable};
+
+pub struct Assembler<'a, T: 'a + fmt::Debug + PartialEq> {
+    pub instruction_table: &'a InstructionTable<T>,
+    pub instructions: Vec<usize>,
+    pub labels: WriteOnceTable<usize>,
+    pub data: Vec<T>,
+}
+
+impl<'a, T: 'a + fmt::Debug + PartialEq> Assembler<'a, T> {
+    pub fn new(it: &'a InstructionTable<T>) -> Self {
+        let mut labels: WriteOnceTable<usize> = WriteOnceTable::new();
+        labels.insert("main", 0);
+
+        Assembler {
+            instruction_table: it,
+            instructions: vec![],
+            labels,
+            data: vec![],
+        }
+    }
+
+    pub fn push_data(&mut self, data: T) -> usize {
+        let pos = self.data.iter().position(|d| d == &data);
+        match pos {
+            Some(pos) => pos,
+            None => {
+                self.data.push(data);
+                self.data.len() - 1
+            }
+        }
+    }
+
+    pub fn push(&mut self, op_name: &str, args: Vec<T>) {
+        let instruction = self
+            .instruction_table
+            .by_op_name(op_name)
+            .unwrap_or_else(|| panic!("Error: the op_name {} doesnt exist", op_name));
+
+        if args.len() != instruction.arity {
+            panic!(
+                "Error: instruction {} has arity of {}, but provided {} args",
+                op_name,
+                instruction.arity,
+                args.len(),
+            );
+        }
+
+        self.instructions.push(instruction.op_code);
+        self.instructions.push(instruction.arity);
+
+        for arg in args {
+            let pos = self.push_data(arg);
+            self.instructions.push(pos);
+        }
+    }
+
+    pub fn label(&mut self, op_name: &str) {
+        self.labels.insert(op_name, self.instructions.len());
+    }
+}
