1eac55b82538d08cde944e71128dbf426cc509fa
willyrgf <willyr.goncalves@gmail.com>
2022-02-12 18:21:20 -0300

    implement a init for write_many_table

--- src/write_many_table.rs
+++ src/write_many_table.rs
@@ -3,3 +3,14 @@ use std::collections::HashMap;
 
 #[derive(Debug, Default)]
 pub struct WriteManyTable<T>(HashMap<String, T>);
+
+impl<T> WriteManyTable<T> {
+    pub fn new() -> Self {
+        WriteManyTable(HashMap::new())
+    }
+
+    pub fn insert(&mut self, name: &str, value: T) {
+        let name = String::from(name);
+        self.0.insert(name, value);
+    }
+}
