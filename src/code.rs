e90287b491f5b6973394cb5df5c306417cd10750
willyrgf <willyr.goncalves@gmail.com>
2022-02-13 12:49:56 -0300

    impl from trait from assembler for code

--- src/code.rs
+++ src/code.rs
@@ -1,6 +1,30 @@
+use crate::assembler::{self, Assembler};
+use std::fmt;
+
 pub struct Code<T> {
     pub symbols: Vec<(usize, String)>,
     pub code: Vec<usize>,
     pub data: Vec<T>,
     pub labels: Vec<(usize, String)>,
+}
+
+impl<'a, T: 'a + fmt::Debug + PartialEq> From<Assembler<'a, T>> for Code<T> {
+    fn from(assembler: Assembler<T>) -> Self {
+        let symbols = assembler.instruction_table.symbols();
+        let code = assembler.instructions;
+        let data = assembler.data;
+        let mut labels: Vec<(usize, String)> = vec![];
+
+        for key in assembler.labels.keys() {
+            let idx = assembler.labels.get(&key).unwrap();
+            labels.push((*idx, key));
+        }
+
+        Code {
+            symbols,
+            code,
+            data,
+            labels,
+        }
+    }
 }
