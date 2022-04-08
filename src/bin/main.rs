6726a6c3929eec65cb2ace954aacd3157d38d626
willyrgf <willyr.goncalves@gmail.com>
2022-04-07 21:51:10 -0300

    [wip] add s-expression support to the vm

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,33 @@
 use env_logger::{Builder, Env};
 use stack_vm::program::Program;
-// use stack_vm
 
 fn main() {
     Builder::from_env(Env::default().default_filter_or("debug")).init();
+
+    let s_exp = sexp::parse(r#"((+ 5 2) (print "hello"))"#).unwrap();
+
+    //TODO: implement a s-express parser in another crate
+    if let sexp::Sexp::List(l) = s_exp {
+        log::debug!("l: {:?}", l);
+
+        // FIXME: do it recursively in compiler module
+        l.iter().for_each(|t| match t {
+            sexp::Sexp::List(ll) => {
+                log::debug!("ll: {:?}", ll);
+                ll.iter().for_each(|tt| match tt {
+                    sexp::Sexp::List(lll) => {
+                        log::debug!("lll: {:?}", lll);
+                    }
+                    sexp::Sexp::Atom(aaa) => {
+                        log::debug!("aaa: {:?}", aaa);
+                    }
+                })
+            }
+            sexp::Sexp::Atom(aa) => {
+                log::debug!("aa: {:?}", aa);
+            }
+        });
+    }
 
     let mut program = Program::new();
     let result = program.exec("");
