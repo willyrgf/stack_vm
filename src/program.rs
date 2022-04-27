b81b6da84f2a5921f502c81cb475c0203a5691aa
willyrgf <willyr.goncalves@gmail.com>
2022-04-27 11:58:53 -0300

    add comparation operation support

--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::ops::Add;
 
 use crate::{
@@ -12,6 +11,13 @@ const STACK_BEGIN: usize = 0;
 macro_rules! binary_op {
     ($stack:expr, $v1:expr, $v2:expr, $op:tt) => {
         $stack.push(Value::Number($v1 $op $v2))
+    };
+}
+
+macro_rules! comparison_op {
+    ($stack:expr, $v1:expr, $v2:expr, $op:tt) => {
+        let b = if $v1 $op $v2 { true } else { false };
+        $stack.push(Value::from(b))
     };
 }
 
@@ -78,43 +84,34 @@ impl Program {
     }
 
     pub fn exec(&mut self, program_code: &str) -> Value {
-        // 1. parser the program
-        // let ast = parser(program);
-        // 2. compile the program to bytecode
-        // let code = compile(ast);
-
-        // self.constants.push(Value::Number(10_f64));
-        // self.constants.push(Value::Number(2_f64));
-
         //TODO: implement a s-expression parser in another crate
         let s_expressions = sexp::parse(program_code).unwrap();
 
-        // TODO: use this code for eval
         let compiled_code = compiler::compile(s_expressions);
 
         self.constants = compiled_code.constants();
         self.code = compiled_code.code();
 
-        let v = vec![
-            Value::String("Hello, ".to_string()),
-            Value::String("World".to_string()),
-        ];
-        // self.constants.push(Value::String("Hello, ".to_string()));
-        // self.constants.push(Value::String("World".to_string()));
+        // let v = vec![
+        //     Value::String("Hello, ".to_string()),
+        //     Value::String("World".to_string()),
+        // ];
+        // // self.constants.push(Value::String("Hello, ".to_string()));
+        // // self.constants.push(Value::String("World".to_string()));
 
-        let c = vec![
-            opcode::OP_CONST,
-            0,
-            opcode::OP_CONST,
-            1,
-            opcode::OP_ADD,
-            opcode::OP_HALT,
-        ];
+        // let c = vec![
+        //     opcode::OP_CONST,
+        //     0,
+        //     opcode::OP_CONST,
+        //     1,
+        //     opcode::OP_ADD,
+        //     opcode::OP_HALT,
+        // ];
 
-        log::debug!("program(): c expectd: {:?}", c);
-        log::debug!("program(): v expectd: {:?}", v);
-        log::debug!("program(): self.code: {:?}", self.code);
-        log::debug!("program(): self.constants: {:?}", self.constants);
+        // log::debug!("program(): c expectd: {:?}", c);
+        // log::debug!("program(): v expectd: {:?}", v);
+        // log::debug!("program(): self.code: {:?}", self.code);
+        // log::debug!("program(): self.constants: {:?}", self.constants);
 
         self.eval()
     }
@@ -202,6 +199,120 @@ impl Program {
                     }
 
                     binary_op!(self, value1.as_number(), value2.as_number(), /)
+                }
+                opcode::OP_EQ => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), ==);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), ==);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), ==);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
+                }
+                opcode::OP_NE => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), !=);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), !=);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), !=);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
+                }
+                opcode::OP_GT => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), >);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), >);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), >);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
+                }
+                opcode::OP_LT => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), <);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), <);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), <);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
+                }
+                opcode::OP_GE => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), >=);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), >=);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), >=);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
+                }
+                opcode::OP_LE => {
+                    log::debug!("eval(): OP_EQ, program: {:?}", self.code);
+
+                    let value2 = self.pop();
+                    let value1 = self.pop();
+
+                    //TODO: remove this if chain to use tuples match
+                    if opcode::are_values_numbers(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_number(), value2.as_number(), <=);
+                    } else if opcode::are_values_strings(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_string(), value2.as_string(), <=);
+                    } else if opcode::are_values_bools(vec![&value1, &value2]) {
+                        comparison_op!(self, value1.as_bool(), value2.as_bool(), <=);
+                    } else {
+                        log::debug!("value1: {:?}, value2: {:?}", value1, value2);
+                        log::error!("eval(): OP_EQ, this operation only supports: Value::Number, Value::String, Value::Bool");
+                        panic!()
+                    }
                 }
                 _ => {
                     log::error!("eval(): opcode doesnt exist, program: {:?}", self);
